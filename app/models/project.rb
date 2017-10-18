b2b79160c2421753fc8885b3e64fb5b36ca51250
Jaime González <jgonzalez1@uc.cl>
2017-10-18 18:46:59 -0300

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,11 @@
 class Project < ApplicationRecord
   belongs_to :user
-  has_many: reviews
-  has_many: methodologys
-  has_one: project_connection
-  has_one: project_implementation
-  has_one: project_communication
-  has_one: project_evaluation
+  has_many :reviews
+  has_many :methodologys
+  has_one :project_connection
+  has_one :project_implementation
+  has_one :project_communication
+  has_one :project_evaluation
   def get_data_resume()
   end
 end
