aecedc21561240215b30e6bbae8e8b0b039b8ea7
gdburky <gdburky@uc.cl>
2017-10-18 18:21:50 -0300

    user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "Example User",
+                     name: "Example Name",
+                     last
+                     email: "user@example.com")
+  end
 end
