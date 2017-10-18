2654c804d11da3fc4c7f8dc735081631c753682b
charly <cjsalame@uc.cl>
2017-10-18 18:32:36 -0300

    devise basics installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  resources :project_communications
-  resources :project_evaluations
-  resources :project_implementations
-  resources :project_connections
+
+  devise_for :users
+  root 'home#index'
+
+
+  resources :projects do
+    resources :project_communications
+    resources :project_evaluations
+    resources :project_implementations
+    resources :project_connections
+    resources :project_reviews
+  end
+  
   resources :methodology_reviews
   resources :methodologies
-  resources :project_reviews
-  resources :projects
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
