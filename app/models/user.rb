c7bf4c9eb39ed08e83a518c67bed1e7104a15bca
Jaime González <jgonzalez1@uc.cl>
2017-10-18 18:48:27 -0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many: projects
-    has_many: project_reviews
-    has_many: methodology_reviews
+    has_many :projects
+    has_many :project_reviews
+    has_many :methodology_reviews
 
     def review_project()
     end
